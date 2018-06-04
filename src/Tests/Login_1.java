8d01cc68bccb6cc09dbc47c3ace557f4e21fad1c
Makarov Anton <antoxam07@mail.ru>
2018-06-04 10:47:32 +0300

    Running the tests

--- src/Tests/Login_1.java
+++ src/Tests/Login_1.java
@@ -3,11 +3,15 @@ package Tests;
 import io.appium.java_client.ios.IOSDriver;
 import io.appium.java_client.ios.IOSElement;
 
+import static org.junit.Assert.assertTrue;
+import org.apache.commons.lang3.StringUtils;
+import util.Element;
 import util.InitializeDriver;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
 
 public class Login_1 {
 
@@ -21,11 +25,166 @@ public class Login_1 {
         driv = new InitializeDriver("11.3");
         driv.startFlow();
         driver = driv.driver;
+
+        assertTrue(Element.Click(driver, "HelpStart"));
+        assertTrue(Element.Click(driver, "LoginButton"));
     }
 
     @Test
     public void loginFlow() throws Exception {
         System.out.println("\n@Test");
+
+        // CASE 1:
+        // Go back
+        Element.Click(driver, "ForgotButton");
+        Element.Click(driver, "BackBtn");
+        if (Element.Click(driver, "ForgotButton")) {
+            System.out.println("PASSED: Case - Device: Go back");
+        }
+
+        // CASE 2:
+        // Incorrect email
+        Element.SetText(driver, "EmailField", "test.email.ru");
+        if (!Element.IsEnable(driver, "SendButton")) {
+            System.out.println("PASSED: Case - Device: Incorrect email");
+        } else {
+            System.out.println("FAIL: Case - Device: Incorrect email");
+        }
+
+        // CASE 3:
+        // Email length <= 100 (ПО ФТ)
+        String field = StringUtils.repeat("A", 100) + "@test.ru";
+        Element.SetText(driver, "EmailField", field);
+        if (Element.GetText(driver, "EmailField").length() < field.length()) {
+            System.out.println("PASSED: Case - Email length <= 100");
+        } else {
+            System.out.println("FAIL: Case - Email length <= 100");
+        }
+
+        // CASE 4:
+        // Invalid characters in email
+        Element.SetText(driver, "EmailField", "DogNumber#5@932.ru");
+        if (!Element.IsEnable(driver, "SendButton")) {
+            System.out.println("PASSED: Case - Invalid characters in email");
+        } else {
+            System.out.println("FAIL: Case - Invalid characters in email");
+        }
+
+        // CASE 5:
+        // Email field is empty
+        Element.SetText(driver, "EmailField", "");
+        if (!Element.IsEnable(driver, "SendButton")) {
+            System.out.println("PASSED: Case - Email field is empty");
+        } else {
+            System.out.println("FAIL: Case - Email field is empty");
+        }
+
+        // CASE 6:
+        // Reset password (wrong email)
+        Element.SetText(driver, "EmailField", "test@0079.ru");
+        Element.Click(driver, "SendButton");
+        if (!Element.isPresent(driver, "OkReset")) {
+            System.out.println("PASSED: Case - Device: Reset password (wrong email)");
+        } else {
+            System.out.println("FAIL: Case - Device: Reset password (wrong email)");
+        }
+
+        // CASE 7:
+        // Reset password (exist email)
+        Element.SetText(driver, "EmailField", "test@0078.ru");
+        Element.Click(driver, "SendButton");
+        if (Element.isPresent(driver, "OkReset")) {
+            Element.Click(driver, "OkReset");
+            System.out.println("PASSED: Case - Device: Reset password (exist email)");
+        } else {
+            System.out.println("FAIL: Case - Device: Reset password (exist email)");
+        }
+
+        // CASE 8:
+        // Entering non-existent mail
+        Element.SetText(driver, "EmailField", "test@0080.ru");
+        Element.SetText(driver, "PasswordLoginField", "password");
+        assertTrue(Element.Click(driver, "GoButton"));
+        if (!Element.isPresent(driver, "SerialNumberField")) {
+            System.out.println("PASSED: Case - Entering non-existent mail");
+        } else {
+            System.out.println("FAIL: Case - Entering non-existent mail");
+            Element.Click(driver, "BackBtn");
+        }
+
+        // CASE 9:
+        // Incorrect password for existing mail
+        Element.SetText(driver, "EmailField", "test@932.ru");
+        Element.SetText(driver, "PasswordLoginField", "1234");
+        assertTrue(Element.Click(driver, "GoButton"));
+        if (!Element.isPresent(driver, "SerialNumberField")) {
+            System.out.println("PASSED: Case - Incorrect password for existing mail");
+        } else {
+            System.out.println("FAIL: Case - Incorrect password for existing mail");
+            Element.Click(driver, "BackBtn");
+        }
+
+        // CASE 10:
+        // Enter a nonexistent password
+        Element.SetText(driver, "EmailField", "test@932.ru");
+        Element.SetText(driver, "PasswordLoginField", "qw12");
+        assertTrue(Element.Click(driver, "GoButton"));
+        if (!Element.isPresent(driver, "SerialNumberField")) {
+            System.out.println("PASSED: Case - Enter a nonexistent password");
+        } else {
+            System.out.println("FAIL: Case - Enter a nonexistent password");
+            Element.Click(driver, "BackBtn");
+        }
+
+
+        Element.SetText(driver, "EmailField", "test@0078.ru");
+        Element.SetText(driver, "PasswordLoginField", "password");
+        assertTrue(Element.Click(driver, "GoButton"));
+
+
+        /// Device registration
+
+        // CASE 11:
+        // One empty field
+        Element.SetText(driver, "SerialNumberField", "621010002");
+        if (!Element.IsEnable(driver, "GoButton")) {
+            System.out.println("PASSED: Case - Device: One empty field");
+        } else {
+            System.out.println("FAIL: Case - Device: One empty field");
+        }
+
+        // CASE 12:
+        // Invalid characters in serial number fields
+        Element.SetText(driver, "SerialNumberField", "621!0@0a");
+        Element.SetText(driver, "PasswordActiveField", "123#");
+        if (!Element.IsEnable(driver, "GoButton")) {
+            System.out.println("PASSED: Case - Device: Invalid characters in serial number/password");
+        } else {
+            System.out.println("FAIL: Case - Device: Invalid characters in serial number/password");
+        }
+
+        // CASE 13:
+        // Serial/password length <= 9, 6
+        field = StringUtils.repeat("1", 10);
+        Element.SetText(driver, "SerialNumberField", field);
+        Element.SetText(driver, "PasswordActiveField", field);
+
+        if (Element.GetText(driver, "SerialNumberField").length() < field.length() &&
+                Element.GetText(driver, "PasswordActiveField").length() < field.length()) {
+            System.out.println("PASSED: Case - Device: Serial/password length <= 9, 6");
+        } else {
+            System.out.println("FAIL: Case - Device: Serial/password length <= 9, 6");
+        }
+
+        // CASE 14:
+        // Open Quick Guide
+        assertTrue(Element.Click(driver, "GuideButton"));
+        if (Element.isPresent(driver, "GuideImage")) {
+            Element.Click(driver, "CloseGuideButton");
+            System.out.println("PASSED: Case - Device: Open Quick Guide");
+        } else {
+            System.out.println("FAIL: Case - Device: Open Quick Guide");
+        }
     }
 
     @After
--- src/Tests/Registration.java
+++ src/Tests/Registration.java
@@ -3,11 +3,15 @@ package Tests;
 import io.appium.java_client.ios.IOSDriver;
 import io.appium.java_client.ios.IOSElement;
 
+import org.apache.commons.lang3.StringUtils;
+import util.Element;
 import util.InitializeDriver;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
 
 public class Registration {
 
@@ -26,6 +30,167 @@ public class Registration {
     @Test
     public void registrationFlow() throws Exception {
         System.out.println("\n@Test");
+
+
+        // CASE 1:
+        // One empty field
+        Element.SetText(driver, "PasswordField", "email");
+        Element.SetText(driver, "ConfirmPasswordField", "email");
+        assertTrue(Element.Click(driver, "UACheckBox"));
+        if (!Element.IsEnable(driver, "GoButton")) {
+            System.out.println("PASSED: Case - One empty field");
+        } else {
+            System.out.println("FAIL: Case - One empty field");
+        }
+
+        // CASE 2:
+        // Different Passwords
+        Element.SetText(driver, "EmailField", "test@932.ru");
+        Element.SetText(driver, "PasswordField", "password_1");
+        Element.SetText(driver, "ConfirmPasswordField", "password_2");
+
+        String pass_1 = Element.GetText(driver, "PasswordField");
+        String confirm = Element.GetText(driver, "ConfirmPasswordField");
+
+        Element.Click(driver, "GoButton");
+        if (Element.isPresent(driver, "SerialNumberField")) {
+            System.out.println("FAIL: Case - Different Passwords");
+            Element.Click(driver, "BackButton");
+        } else {
+            System.out.println("PASSED: Case - Different Passwords");
+        }
+
+        // CASE 3:
+        // Inactive checkbox
+        Element.Click(driver, "UACheckBox");
+        if (!Element.IsEnable(driver, "GoButton")) {
+            System.out.println("PASSED: Case - Inactive checkbox");
+            Element.Click(driver, "UACheckBox");
+        } else {
+            System.out.println("FAIL: Case - Inactive checkbox");
+        }
+
+        // CASE 4:
+        // Invalid characters in email
+        Element.SetText(driver, "EmailField", "DogNumber#5@932.ru");
+        if (!Element.IsEnable(driver, "GoButton")) {
+            System.out.println("PASSED: Case - Invalid characters in email");
+        } else {
+            System.out.println("FAIL: Case - Invalid characters in email");
+        }
+
+        // CASE 5:
+        // Invalid email mask
+        Element.SetText(driver, "EmailField", "steingrims@a.m");
+        if (!Element.IsEnable(driver, "GoButton")) {
+            System.out.println("PASSED: Case - Invalid email mask");
+        } else {
+            System.out.println("FAIL: Case - Invalid email mask");
+        }
+
+        // CASE 6:
+        // Email length <= 100 (ПО ФТ)
+        String field = StringUtils.repeat("A", 100) + "@test.ru";
+        Element.SetText(driver, "EmailField",field);
+        if (Element.GetText(driver, "EmailField").length() < field.length()) {
+            System.out.println("PASSED: Case - Email length <= 100");
+            Element.SetText(driver, "EmailField", "test@932.ru");
+        } else {
+            System.out.println("FAIL: Case - Email length <= 100");
+        }
+
+        // CASE 7:
+        // Password length <= 50
+        field = StringUtils.repeat("A", 51);
+        Element.SetText(driver, "PasswordField",field);
+        Element.SetText(driver, "ConfirmPasswordField",field);
+
+        if (Element.GetText(driver, "PasswordField").length() < field.length()
+                && Element.GetText(driver, "ConfirmPasswordField").length() < field.length()) {
+            System.out.println("PASSED: Case - Password length <= 50");
+        } else {
+            System.out.println("FAIL: Case - Password length <= 50");
+        }
+
+        // CASE 8:
+        // Open User Agreement
+        assertTrue(Element.Click(driver, "UserAgreement"));
+        if (Element.isPresent(driver, "UATextView")) {
+            Element.Click(driver, "BackBtn");
+            System.out.println("PASSED: Case - Open User Agreement");
+        } else {
+            System.out.println("FAIL: Case - Open User Agreement");
+        }
+
+        // CASE 9:
+        // Email already in use
+        Element.SetText(driver, "EmailField", "test@932.ru");
+        Element.SetText(driver, "PasswordField", "password");
+        Element.SetText(driver, "ConfirmPasswordField", "password");
+
+        assertTrue(Element.Click(driver, "GoButton"));
+        if (Element.isPresent(driver, "LoginButton")) {
+            System.out.println("PASSED: Case - Email already in use");
+        } else {
+            System.out.println("FAIL: Case - Email already in use");
+        }
+
+        // CASE 10:
+        // Success registration
+        Element.SetText(driver, "EmailField", "test@0078.ru");
+        Element.SetText(driver, "PasswordField", "password");
+        Element.SetText(driver, "ConfirmPasswordField", "password");
+        assertTrue(Element.Click(driver, "GoButton"));
+
+        if (Element.isPresent(driver, "SerialNumberField")) {
+            System.out.println("PASSED: Case - Success registration");
+        } else {
+            System.out.println("FAIL: Case - Success registration");
+        }
+
+        /// Device registration
+
+        // CASE 11:
+        // One empty field
+        Element.SetText(driver, "SerialNumberField", "621010002");
+        if (!Element.IsEnable(driver, "GoButton")) {
+            System.out.println("PASSED: Case - Device: One empty field");
+        } else {
+            System.out.println("FAIL: Case - Device: One empty field");
+        }
+
+        // CASE 12:
+        // Invalid characters in serial number fields
+        Element.SetText(driver, "SerialNumberField", "621!0@0a");
+        Element.SetText(driver, "PasswordActiveField", "123#");
+        if (!Element.IsEnable(driver, "GoButton")) {
+            System.out.println("PASSED: Case - Device: Invalid characters in serial number/password");
+        } else {
+            System.out.println("FAIL: Case - Device: Invalid characters in serial number/password");
+        }
+
+        // CASE 13:
+        // Serial/password length <= 9, 6
+        field = StringUtils.repeat("1", 10);
+        Element.SetText(driver, "SerialNumberField", field);
+        Element.SetText(driver, "PasswordActiveField", field);
+
+        if (Element.GetText(driver, "SerialNumberField").length() < field.length() &&
+                Element.GetText(driver, "PasswordActiveField").length() < field.length()) {
+            System.out.println("PASSED: Case - Device: Serial/password length <= 9, 6");
+        } else {
+            System.out.println("FAIL: Case - Device: Serial/password length <= 9, 6");
+        }
+
+        // CASE 14:
+        // Open Quick Guide
+        assertTrue(Element.Click(driver, "GuideButton"));
+        if (Element.isPresent(driver, "GuideImage")) {
+            Element.Click(driver, "CloseGuideButton");
+            System.out.println("PASSED: Case - Device: Open Quick Guide");
+        } else {
+            System.out.println("FAIL: Case - Device: Open Quick Guide");
+        }
     }
 
     @After
